3bc389dccf062dcd255907796880df47f738722c
Marcin Tomaszewski <tomaszewsky.marcin@gmail.com>
2022-02-08 22:05:20 +0100

    Sum of Two Integers

--- 371_Sum_Of_Two_Integers.py
+++ 371_Sum_Of_Two_Integers.py
@@ -1,17 +1,4 @@
 class Solution:
     def getSum(self, a: int, b: int) -> int:
         
-        bit_length = 32
-        neg_bit, mask = (1 << bit_length) >> 1, ~(~0 << bit_length)
-
-        a = (a | ~mask) if (a & neg_bit) else (a & mask)
-        b = (b | ~mask) if (b & neg_bit) else (b & mask)
-
-        while b:
-            carry = a & b
-            a ^= b
-            a = (a | ~mask) if (a & neg_bit) else (a & mask)
-            b = carry << 1
-            b = (b | ~mask) if (b & neg_bit) else (b & mask)
-
-        return a
+        return(sum([a,b]))
