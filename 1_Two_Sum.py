8d00470d2e55a62afdf7448f017872d6b61789f7
Marcin Tomaszewski <87043088+martom93@users.noreply.github.com>
2022-02-13 22:38:53 +0100

    Two Sum

--- 1_Two_Sum.py
+++ 1_Two_Sum.py
@@ -1,5 +1,6 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        #Hashset
         szukane = {}
         for i in range(len(nums)):
             if nums[i] in szukane:
